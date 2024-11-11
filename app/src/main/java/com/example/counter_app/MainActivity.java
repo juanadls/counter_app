b31e7380adb56df75b12be5d0705d029b5905054
juanadls <eleo.dev09@gmail.com>
2024-11-11 07:30:30 -0300

    added counter functionality

--- app/src/main/java/com/example/counter_app/MainActivity.java
+++ app/src/main/java/com/example/counter_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.counter_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView welcome_text, counter_text;
+    Button btn;
+    int counter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        counter_text = findViewById(R.id.counter_text);
+
+
+        // Adding the Functionalities
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter_text.setText(""+increaseCounter());
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+    }
+
+    public int increaseCounter(){
+        return ++counter;
     }
 }
